31802dfe76ad21aeeb3c9f48291862321d743f40
Asepimam <asepimammutaqin317@gmail.com>
2025-08-24 16:46:43 +0700

    Add CalegMapper interface for mapping between CalegEntity, CalegDto, and Caleg models

---
+++ src/main/java/com/allobank/allobackendtest/mapper/CalegMapper.java
@@ -0,0 +1,32 @@
+package com.allobank.allobackendtest.mapper;
+
+import com.allobank.allobackendtest.dto.CalegDto;
+import com.allobank.allobackendtest.entity.CalegEntity;
+import com.allobank.allobackendtest.model.Caleg;
+import org.mapstruct.*;
+import org.mapstruct.Mapper;
+
+
+@Mapper(componentModel = "spring", uses = {DapilMapper.class, PartaiMapper.class})
+public interface CalegMapper extends BaseMapper<CalegEntity, CalegDto, Caleg> {
+
+    @Override
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dapil", ignore = true) 
+    @Mapping(target = "partai", ignore = true)
+    CalegEntity toEntity(CalegDto dto);
+
+    @Override
+    @Mapping(target = "dapil", source = "dapil")
+    @Mapping(target = "partai", source = "partai")
+    Caleg toResponse(CalegEntity entity);
+
+    @Override
+    @Mapping(target = "dapil", ignore = true)
+    @Mapping(target = "partai", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromRequest(CalegDto dto, @MappingTarget CalegEntity entity);
+}
+
+
+
