500f3e1ebe8c17d4906268c7cabd359607eadde4
Asepimam <asepimammutaqin317@gmail.com>
2025-08-24 21:33:04 +0700

    Enhance CalegRepository to support pagination and specification execution for Caleg entities

--- src/main/java/com/allobank/allobackendtest/repository/CalegRepository.java
+++ src/main/java/com/allobank/allobackendtest/repository/CalegRepository.java
@@ -1,19 +1,25 @@
 package com.allobank.allobackendtest.repository;
 
 import com.allobank.allobackendtest.entity.CalegEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 /**
  * Repository for managing Caleg (Legislative Candidates) entities.
  */
 @Repository
-public interface CalegRepository extends JpaRepository<CalegEntity, UUID> {
+public interface CalegRepository extends JpaRepository<CalegEntity, UUID>, JpaSpecificationExecutor<CalegEntity> {
     
     // Find Calegs by name
     List<CalegEntity> findByNama(String nama);
+
+    boolean existsByDapilIdAndNomorUrut(UUID dapilId, Integer nomorUrut);
 }
