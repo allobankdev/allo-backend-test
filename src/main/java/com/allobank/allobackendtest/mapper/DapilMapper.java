30b5142a265460deb2f0f031c18b0cae461e6177
Asepimam <asepimammutaqin317@gmail.com>
2025-08-24 17:01:18 +0700

    Add DapilMapper interface for mapping Dapil entities to DTOs and vice versa

---
+++ src/main/java/com/allobank/allobackendtest/mapper/DapilMapper.java
@@ -0,0 +1,30 @@
+package com.allobank.allobackendtest.mapper;
+
+import com.allobank.allobackendtest.dto.DapilDto;
+import com.allobank.allobackendtest.dto.DapilUpdateDto;
+import com.allobank.allobackendtest.entity.DapilEntity;
+import com.allobank.allobackendtest.model.Dapil;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * Mapper for Dapil (Electoral District) entities.
+ */
+@Mapper(componentModel = "spring")
+public interface DapilMapper extends BaseMapper<DapilEntity, DapilDto, Dapil> {
+
+    // CREATE request mapping
+    @Override
+    @Mapping(target = "id", ignore=true)
+    DapilEntity toEntity(DapilDto dto);
+
+    // READ request mapping
+    @Override
+    Dapil toResponse(DapilEntity entity);
+
+    // UPDATE request mapping
+    @Override
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromRequest(DapilDto dto, @MappingTarget DapilEntity entity);
+
+}
