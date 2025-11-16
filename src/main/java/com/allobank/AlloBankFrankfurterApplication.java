f8c5f85616bd70a424ba17c334ad9b754a05f0d9
hakimamarullah <hakimamarullah@gmail.com>
2025-11-16 13:55:21 +0700

    chore: init project

---
+++ src/main/java/com/allobank/AlloBankFrankfurterApplication.java
@@ -0,0 +1,13 @@
+package com.allobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloBankFrankfurterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlloBankFrankfurterApplication.class, args);
+	}
+
+}
