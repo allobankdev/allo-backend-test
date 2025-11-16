85012cc4ee9044e967d633b1574983fd74beda90
Ari Aulia <aulia_ari@yahoo.com>
2025-11-16 10:43:08 +0700

    initial commit

---
+++ src/main/java/id/co/microservice/currency/currency_service/CurrencyServiceApplication.java
@@ -0,0 +1,13 @@
+package id.co.microservice.currency.currency_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/microservice/currency/currency_service/CurrencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.microservice.currency.currency_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
