ee9fc235d716c12b3d4ea56acec3a017774e9250
mendochandra <mendochandra112@gmail.com>
2025-11-18 21:42:42 +0700

    feat: Initialize Spring Boot project structure for IDR Rate Aggregator

---
+++ src/main/java/com/example/feat/idr_rate_aggregator/Application.java
@@ -0,0 +1,13 @@
+package com.example.feat.idr_rate_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/feat/idr_rate_aggregator/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feat.idr_rate_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
