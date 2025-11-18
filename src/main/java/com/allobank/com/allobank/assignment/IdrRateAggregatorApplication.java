4ffa8137c20722dbdd17d4f6dd794879df8bab9f
Prayugo <pray.dwi24@gmail.com>
2025-11-19 00:10:39 +0700

    chore: initial project setup
    Initialize Spring Boot project using Maven with base folder structure, default dependencies, and initial configuration files generated from Spring Initializr.

---
+++ src/main/java/com/allobank/com/allobank/assignment/IdrRateAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.allobank.com.allobank.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdrRateAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdrRateAggregatorApplication.class, args);
+	}
+
+}
