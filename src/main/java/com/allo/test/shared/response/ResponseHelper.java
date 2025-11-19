4926b7eec0135b9aeb88d7bff1d2e1ea9b273d8c
frhn9 <just_fadhyl@hotmail.co.id>
2025-11-19 18:07:51 +0700

    chore: remove unused dependencies and clean up build.gradle

--- src/main/java/com/allo/test/shared/response/ResponseHelper.java
+++ src/main/java/com/allo/test/shared/response/ResponseHelper.java
@@ -1,13 +1,10 @@
 package com.allo.test.shared.response;
 
 import com.allo.test.shared.response.attribute.ErrorAttribute;
-import com.allo.test.shared.response.attribute.MetaAttribute;
 import com.allo.test.shared.response.template.ResponseData;
 import com.allo.test.shared.response.template.ResponseError;
 import com.allo.test.shared.response.template.ResponseList;
-import com.allo.test.shared.response.template.ResponseMeta;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -57,19 +54,6 @@ public class ResponseHelper {
             .body(ResponseError.builder()
                     .responseSchema(responseMessageHelper.getResponseSchema(responseEnum))
                     .errors(errorAttributes)
-                    .build());
-  }
-
-  public <T> ResponseEntity<ResponseMeta<T>> createResponseMeta(
-          ResponseEnum responseEnum,
-          Page<T> page
-  ) {
-    return ResponseEntity
-            .status(responseEnum.getHttpStatus())
-            .body(ResponseMeta.<T>builder()
-                    .responseSchema(responseMessageHelper.getResponseSchema(responseEnum))
-                    .meta(MetaAttribute.builder().page(page.getNumber()).size(page.getSize()).total(page.getTotalElements()).build())
-                    .data(page.getContent())
                     .build());
   }
 
