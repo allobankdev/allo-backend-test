5c4d4d8f1070d089cb4e526adb549d602d847167
maulana <faridz.ihza.mahendra@bni.co.id>
2025-11-19 14:03:10 +0700

    initial project

---
+++ src/main/java/com/allobank/AllobankApplication.java
@@ -0,0 +1,13 @@
+package com.allobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllobankApplication.class, args);
+	}
+
+}
