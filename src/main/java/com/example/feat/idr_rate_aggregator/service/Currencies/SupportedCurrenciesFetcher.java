b7cc5d5823892ecef0cdf8a8eab17d0218cebf19
mendochandra <mendochandra112@gmail.com>
2025-11-19 23:34:43 +0700

    feat : Implement Implement Currencies Fetcher strategy

--- src/main/java/com/example/feat/idr_rate_aggregator/service/Currencies/SupportedCurrenciesFetcher.java
+++ src/main/java/com/example/feat/idr_rate_aggregator/service/Currencies/SupportedCurrenciesFetcher.java
@@ -1,19 +1,20 @@
-package com.example.feat.idr_rate_aggregator.service.Historical;
+package com.example.feat.idr_rate_aggregator.service.Currencies;
 
-import com.example.feat.idr_rate_aggregator.dto.HistoricalRatesResponse;
 import com.example.feat.idr_rate_aggregator.exception.ExternalApiException;
 import com.example.feat.idr_rate_aggregator.service.financeDataStore.IDRDataFetcher;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Component("historical_idr_usd")
-public class HistoricalRatesFetcher implements IDRDataFetcher {
+import java.util.Map;
 
-    private static final String RESOURCE_KEY = "historical_idr_usd";
-    private static final String DATE_RANGE_URI = "/2024-01-01..2024-01-05?from=IDR&to=USD";
+@Component("supported_currencies")
+public class SupportedCurrenciesFetcher implements IDRDataFetcher {
+
+    private static final String RESOURCE_KEY = "supported_currencies";
     private final WebClient webClient;
 
-    public HistoricalRatesFetcher(WebClient webClient) {
+    public SupportedCurrenciesFetcher(WebClient webClient) {
         this.webClient = webClient;
     }
 
@@ -24,14 +25,18 @@ public class HistoricalRatesFetcher implements IDRDataFetcher {
 
     @Override
     public Object fetchData() {
+
+        ParameterizedTypeReference<Map<String, String>> typeRef =
+                new ParameterizedTypeReference<Map<String, String>>() {};
+
         return webClient.get()
-                .uri(DATE_RANGE_URI)
+                .uri("/currencies")
                 .retrieve()
                 .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(),
                         clientResponse -> clientResponse.bodyToMono(String.class).flatMap(body ->
-                                reactor.core.publisher.Mono.error(new ExternalApiException("Frankfurter API error: " + body))
+                                reactor.core.publisher.Mono.error(new ExternalApiException("Frankfurter error: " + body))
                         ))
-                .bodyToMono(HistoricalRatesResponse.class)
+                .bodyToMono(typeRef)
                 .block();
     }
 }
