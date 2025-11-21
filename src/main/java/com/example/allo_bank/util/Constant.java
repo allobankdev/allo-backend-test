c0048558bb2f9fc157063682af2f55fe73d4fd26
LAPTOP-VM4F6N3B\Luky <lukypancap@gmail.com>
2025-11-21 12:23:42 +0700

    feat: Implement test case for RunnerApp and Add IDR on constant

--- src/main/java/com/example/allo_bank/util/Constant.java
+++ src/main/java/com/example/allo_bank/util/Constant.java
@@ -3,6 +3,7 @@ package com.example.allo_bank.util;
 public class Constant {
 
     public static final String USD = "USD";
+    public static final String IDR = "IDR";
     public static final String SUCCESS = "success";
 
 }
