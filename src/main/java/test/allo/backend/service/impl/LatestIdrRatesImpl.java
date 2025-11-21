a9a512eea9d765dddf25633cf79fa99a88e6ad76
pramudiaPutra <sarcastrope@gmail.com>
2025-11-21 09:59:56 +0700

    feat(strategy): implement latest IDR rates

--- src/main/java/test/allo/backend/service/impl/LatestIdrRatesImpl.java
+++ src/main/java/test/allo/backend/service/impl/LatestIdrRatesImpl.java
@@ -4,19 +4,60 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import test.allo.backend.service.IDRDataFetcher;
 
+@Slf4j
 @Service("latest_idr_rates")
 @RequiredArgsConstructor
 public class LatestIdrRatesImpl implements IDRDataFetcher {
 
+    @Value("${internal.data.github-username}")
+    String username;
+
     private final ObjectMapper mapper;
+    private final WebClient webClient;
 
     @Override
     public JsonNode fetchData() {
-        ObjectNode node = mapper.createObjectNode();
-        node.put("name", "latest_idr_rates");
-        return node;
+
+        int unicodeSum = 0;
+        double rateUsd = 0d;
+
+        log.info("username value: {}", username);
+        for(char c : username.toLowerCase().toCharArray()) unicodeSum += c;
+
+        double spreadFactor = (unicodeSum % 1000) / 100000.0;
+        log.info("spreadFactor value: {}", spreadFactor);
+
+        JsonNode externalResponse = webClient.get()
+                .uri("/latest?base=IDR")
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .block();
+        log.info("external API response: {}", externalResponse);
+
+        if(externalResponse != null && externalResponse.has("rates")) {
+            rateUsd = externalResponse
+                    .path("rates")
+                    .path("USD")
+                    .asDouble(0.0);
+        }
+        log.info("rateUsd value: {}", rateUsd);
+
+        double usbBuySpreadIdr = (1 / rateUsd) * (1 + spreadFactor);
+        log.info("usbBuySpreadIdr value: {}", usbBuySpreadIdr);
+
+        ObjectNode response = mapper.createObjectNode();
+        response.put("username", username.toLowerCase());
+        response.put("unicode", unicodeSum);
+        response.put("spreadFactor", spreadFactor);
+        response.put("rateUsd", rateUsd);
+        response.put("usdBuySpreadIdr", usbBuySpreadIdr);
+
+        return response;
     }
 }
