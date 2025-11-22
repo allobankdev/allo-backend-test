a34a6438985f337b1fb74f78a99e3572afcf6f3c
ChikoHakles <>
2025-11-22 15:35:31 +0700

    [feat/idr-usd-agregator] Add : validator for resource type

--- src/main/java/com/chikohakles/allobank/agregator/service/impl/AgregatorServiceImpl.java
+++ src/main/java/com/chikohakles/allobank/agregator/service/impl/AgregatorServiceImpl.java
@@ -4,6 +4,7 @@ import com.chikohakles.allobank.agregator.constant.ResourceType;
 import com.chikohakles.allobank.agregator.service.AgregatorService;
 import com.chikohakles.allobank.agregator.strategy.BaseStrategy;
 import com.chikohakles.allobank.agregator.strategy.BaseStrategyFactory;
+import com.chikohakles.allobank.agregator.validator.ResourceTypeValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AgregatorServiceImpl implements AgregatorService {
     private final BaseStrategyFactory baseStrategyFactory;
+    private final ResourceTypeValidator resourceTypeValidator;
     @Override
     public Object getData(String resourceType) {
+        resourceTypeValidator.validate(resourceType.toUpperCase());
         ResourceType resource = ResourceType.valueOf(resourceType.toUpperCase());
         BaseStrategy baseStrategy = baseStrategyFactory.getStrategy(resource);
         return baseStrategy.getData();
---
+++ src/main/java/com/chikohakles/allobank/agregator/validator/ResourceTypeValidator.java
@@ -0,0 +1,17 @@
+package com.chikohakles.allobank.agregator.validator;
+
+import com.chikohakles.allobank.agregator.constant.ResourceType;
+import io.micrometer.common.util.StringUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResourceTypeValidator {
+    public void validate(String resourceType) {
+        if (!StringUtils.isBlank(resourceType)) {
+            throw new IllegalArgumentException(resourceType + " is not a valid resource type.");
+        }
+
+        //the .fromCode() will always throw an exception if there's no enum matched with given code
+        ResourceType.fromCode(resourceType);
+    }
+}
