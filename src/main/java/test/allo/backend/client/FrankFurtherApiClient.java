8350e1a6dc9fafa1d17f9ae67e06b58baecf43e5
pramudia <sarcastrope@gmail.com>
2025-11-22 14:45:27 +0700

    refactor(api): centralized api call function and add exception handling

--- src/main/java/test/allo/backend/client/FrankFurtherApiClient.java
+++ src/main/java/test/allo/backend/client/FrankFurtherApiClient.java
@@ -1,11 +1,16 @@
 package test.allo.backend.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Slf4j
 @Service
@@ -22,37 +27,44 @@ public class FrankFurtherApiClient {
     String uriSupportedCurrency;
 
     private final WebClient webClient;
+    private final ObjectMapper mapper;
 
     public JsonNode fetchLatestIdrRates() {
-        JsonNode latestIdrRates = webClient.get()
-                .uri(uriLatestIdrRate)
-                .retrieve()
-                .bodyToMono(JsonNode.class)
-                .block();
-
-        log.info("latestIdrRates response: {}", latestIdrRates);
-        return latestIdrRates;
+        return fetchData(uriLatestIdrRate, "latestIdrRate");
     }
 
     public JsonNode fetchHistoricalIdrUsd() {
-        JsonNode historicalIdrUsd = webClient.get()
-                .uri(uriHistoricalIdrUsd)
-                .retrieve()
-                .bodyToMono(JsonNode.class)
-                .block();
-
-        log.info("HistoricalIdrUsd response: {}", historicalIdrUsd);
-        return historicalIdrUsd;
+        return fetchData(uriHistoricalIdrUsd, "historicalIdrUsd");
     }
 
     public JsonNode fetchSupportedCurrencies() {
-        JsonNode supportedCurrencies = webClient.get()
-                .uri(uriSupportedCurrency)
-                .retrieve()
-                .bodyToMono(JsonNode.class)
-                .block();
+        return fetchData(uriSupportedCurrency, "supportedCurrencies");
+    }
 
-        log.info("supportedCurrencies response: {}", supportedCurrencies);
-        return supportedCurrencies;
+    private JsonNode fetchData(String uri, String responseId) {
+        try {
+            JsonNode supportedCurrencies = webClient.get()
+                    .uri(uri)
+                    .retrieve()
+                    .bodyToMono(JsonNode.class)
+                    .block();
+
+            log.info("{} response: {}", responseId, supportedCurrencies);
+            return supportedCurrencies;
+        } catch (WebClientResponseException e) {
+            return failedResponse(e.getStatusCode().value(), "HTTP error", e.getResponseBodyAsString());
+        } catch (WebClientRequestException e) {
+            return failedResponse(503, "Service Unreachable", e.getMessage());
+        } catch (WebClientException e) {
+            return failedResponse(500, "Unexpected Error", e.getMessage());
+        }
+    }
+
+    private ObjectNode failedResponse(int responseCode, String error, String message) {
+        ObjectNode response = mapper.createObjectNode();
+        response.put("status", responseCode);
+        response.put("error", error);
+        response.put("detail", message != null && !message.isEmpty() ? message : "Unknown");
+        return response;
     }
 }
