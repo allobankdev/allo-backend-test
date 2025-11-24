f2233563b1d6af4d780b873240f3f57d6fb0a620
Schwanzeirs <yatamisaki342@gmail.com>
2025-11-24 15:52:50 +0700

    chore: initial spring boot maven project structure

---
+++ allo-bank-test-exhange-rate/src/main/java/com/test/allo_bank_test_exhange_rate/AlloBankTestExhangeRateApplication.java
@@ -0,0 +1,13 @@
+package com.test.allo_bank_test_exhange_rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloBankTestExhangeRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlloBankTestExhangeRateApplication.class, args);
+	}
+
+}
