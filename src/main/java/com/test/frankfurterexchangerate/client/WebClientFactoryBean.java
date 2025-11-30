aee12041095d045003f7e7518f81f54aedd558d5
masbayuh <59537202+masbayuh@users.noreply.github.com>
2025-11-30 20:53:04 +0700

    Finance Frankfurter Exchange rates

---
+++ src/main/java/com/test/frankfurterexchangerate/client/WebClientFactoryBean.java
@@ -0,0 +1,30 @@
+package com.test.frankfurterexchangerate.client;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+//@Component
+public class WebClientFactoryBean  {
+//    @Value("${frankfurter.base-url}")
+//    private String baseUrl;
+//
+//    @Override
+//    public WebClient getObject() {
+//        return WebClient.builder()
+//                .baseUrl(baseUrl)
+//                .build();
+//    }
+//
+//    @Override
+//    public Class<?> getObjectType() {
+//        return WebClient.class;
+//    }
+//
+//    @Override
+//    public boolean isSingleton() {
+//        return true;
+//    }
+}
