089d560482ff9ba29ebe2f39fb4e95db9ed4c351
Devins11 <devinfcb42@gmail.com>
2025-12-01 22:28:59 +0700

    feat(Dto): added a DTO class for response body

---
+++ idr-rate-aggregator/src/main/java/com/app/dto/ApiClientResponseDto.java
@@ -0,0 +1,30 @@
+package com.app.dto;
+
+
+public class ApiClientResponseDto<T> {
+
+    private String status;
+
+    private T data;
+
+    public ApiClientResponseDto(String status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
