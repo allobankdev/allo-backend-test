1ae73381da8f634bd26c55067028cce532e9ce80
Achlaq Al Arif <achlaqalarif@yahoo.co.id>
2025-12-05 14:53:27 +0700

    chore: initialize spring boot project

---
+++ src/test/java/achlaq/co/allo_backend_test/AlloBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package achlaq.co.allo_backend_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlloBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
