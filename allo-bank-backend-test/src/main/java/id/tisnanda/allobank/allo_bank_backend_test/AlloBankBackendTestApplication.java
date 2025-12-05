f3205c54db197a806439c3684b5edd5b2c0a49ee
Trisnanda Nur Hidayat <trisnanda.hidayat@ifg-life.id>
2025-12-05 23:07:57 +0700

    feat : initialize Spring Boot project with basic structure

---
+++ allo-bank-backend-test/src/main/java/id/tisnanda/allobank/allo_bank_backend_test/AlloBankBackendTestApplication.java
@@ -0,0 +1,13 @@
+package id.tisnanda.allobank.allo_bank_backend_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloBankBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlloBankBackendTestApplication.class, args);
+	}
+
+}
---
+++ allo-bank-backend-test/src/test/java/id/tisnanda/allobank/allo_bank_backend_test/AlloBankBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package id.tisnanda.allobank.allo_bank_backend_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlloBankBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
