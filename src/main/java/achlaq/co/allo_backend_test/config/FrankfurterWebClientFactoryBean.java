6a6ee7593fa1917874b9703f1dafa37947f3ed55
achlaq <achlaq.arif@muf.co.id>
2025-12-14 21:13:07 +0700

    feat: update factory bean and yaml

--- src/main/java/achlaq/co/allo_backend_test/config/FrankfurterWebClientFactoryBean.java
+++ src/main/java/achlaq/co/allo_backend_test/config/FrankfurterWebClientFactoryBean.java
@@ -1,9 +1,16 @@
 package achlaq.co.allo_backend_test.config;
 
+import io.netty.channel.ChannelOption;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Component
 @RequiredArgsConstructor
@@ -15,8 +22,15 @@ public class FrankfurterWebClientFactoryBean implements FactoryBean<WebClient> {
     @Override
     public WebClient getObject() {
         if (webClient == null) {
+            HttpClient httpClient = HttpClient.create()
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, props.getConnectTimeoutMs())
+                    .responseTimeout(Duration.ofMillis(props.getReadTimeoutMs()));
+
             webClient = WebClient.builder()
                     .baseUrl(props.getBaseUrl())
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                    .clientConnector(new ReactorClientHttpConnector(httpClient))
                     .build();
         }
         return webClient;
