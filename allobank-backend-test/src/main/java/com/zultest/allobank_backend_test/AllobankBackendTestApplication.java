27a1b2ef544150e95991edbd1942554e79fc5450
laptop11\laptop11 <kzulfazriawan@gmail.com>
2025-12-19 13:33:23 +0700

    Initial development env and properties

---
+++ allobank-backend-test/src/main/java/com/zultest/allobank_backend_test/AllobankBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.zultest.allobank_backend_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllobankBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllobankBackendTestApplication.class, args);
+	}
+
+}
