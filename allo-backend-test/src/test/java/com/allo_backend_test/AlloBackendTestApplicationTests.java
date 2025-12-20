889032e266f08b357ffcf7fd1ca0ed3715e1862b
Happid <happidilmi@gmail.com>
2025-12-20 11:06:14 +0700

    init: first push, initial project using https://start.spring.io/

---
+++ allo-backend-test/src/test/java/com/allo_backend_test/AlloBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allo_backend_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlloBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
