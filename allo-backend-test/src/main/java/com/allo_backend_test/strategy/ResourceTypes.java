99fb93d74333f6df5fe8642c26ce12cbf700f6f0
Happid <happidilmi@gmail.com>
2025-12-20 11:47:46 +0700

    feat: add ResourceType for avoid typo

---
+++ allo-backend-test/src/main/java/com/allo_backend_test/strategy/ResourceTypes.java
@@ -0,0 +1,9 @@
+package com.allo_backend_test.strategy;
+
+public final class ResourceTypes {
+    private ResourceTypes() {}
+
+    public static final String LATEST_IDR_RATES = "latest_idr_rates";
+    public static final String HISTORICAL_IDR_USD = "historical_idr_usd";
+    public static final String SUPPORTED_CURRENCIES = "supported_currencies";
+}
