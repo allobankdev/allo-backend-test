3bff88d8bc79a038c43cfb4e291bcfb935e6ec22
hasan almunawar <109322433+hasanalmunawr@users.noreply.github.com>
2026-01-05 20:09:18 +0700

    chore: initial spring boot setup

---
+++ src/main/java/com/allobank/allobanktest/AllobanktestApplication.java
@@ -0,0 +1,13 @@
+package com.allobank.allobanktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllobanktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllobanktestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allobank/allobanktest/AllobanktestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allobank.allobanktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllobanktestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
