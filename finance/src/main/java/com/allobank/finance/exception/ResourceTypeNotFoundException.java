da0444d9cee140e5274694b0eccfc4a3754be565
hizkiarenat <renathizkia16@gmail.com>
2026-01-15 10:36:14 +0700

    add ResourceTypeNotFoundException

--- finance/src/main/java/com/allobank/finance/exception/ResourceTypeNotFoundException.java
+++ finance/src/main/java/com/allobank/finance/exception/ResourceTypeNotFoundException.java
@@ -2,4 +2,13 @@ package com.allobank.finance.exception;
 
 public class ResourceTypeNotFoundException extends RuntimeException{
 
+ 
+    public ResourceTypeNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceTypeNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
