8ae1b7b9bec5e85bcc289212149184b091f2a0bd
10094365 - Ilham Harazki Polem <ilham.polem@adira.co.id>
2026-01-19 17:31:21 +0700

    Initialize Spring Boot project with Maven, including all necessary configuration files and initial setup.

---
+++ src/main/java/com/example/allo/AlloApplication.java
@@ -0,0 +1,13 @@
+package com.example.allo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/allo/AlloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
