dd6138c909086d9c42724bb872c98015517fccd1
DewaSRY <sdewa6645@gmail.com>
2026-01-24 19:05:47 +0800

    fix: data format case

--- src/main/java/com/sdewa/IdrRateAggregator/dtoes/HistoricalIdrUsdResponse.java
+++ src/main/java/com/sdewa/IdrRateAggregator/dtoes/HistoricalIdrUsdResponse.java
@@ -2,6 +2,8 @@ package com.sdewa.IdrRateAggregator.dtoes;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +14,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HistoricalIdrUsdResponse {
-        private double amount;
+    private double amount;
     private String base;
+    @JsonProperty("start_date")
     private String startDate;
+    @JsonProperty("end_date")
     private String endDate;
-    private Map<String, Map<String, Double>> rates; 
+    private Map<String, Map<String, Double>> rates;
 }
