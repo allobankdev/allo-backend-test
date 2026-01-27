e6a58791a123109ad9a9ed3ce68eb3b13ae176f5
hendrowunga <hendrowunga073@gmail.com>
2026-01-27 13:35:42 +0700

    test: add unit tests for strategies and integration tests for data loading

--- backend/src/test/java/com/hend/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/hend/backend/BackendApplicationTests.java
@@ -1,13 +1,39 @@
 package com.hend.backend;
 
+import com.hend.backend.service.FinanceDataStorage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+/**
+ * @author : hend wunga
+ */
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+		// Port 0 berarti acak, ${wiremock.server.port} akan mengambil port tersebut
+		"app.frankfurter.base-url=http://localhost:${wiremock.server.port}"
+})
+@AutoConfigureWireMock(port = 0)
 class BackendApplicationTests {
+
+	@Autowired
+	private FinanceDataStorage storage;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testDataInjectedOnStartup() {
+		// Karena kita menggunakan file di /mappings, data pasti sudah terisi
+		// saat ApplicationRunner berjalan di awal.
+		assertNotNull(storage.getData("latest_idr_rates"), "Data Latest IDR tidak boleh null");
+		assertNotNull(storage.getData("historical_idr_usd"), "Data Historical tidak boleh null");
+		assertNotNull(storage.getData("supported_currencies"), "Data Currencies tidak boleh null");
+	}
 }
