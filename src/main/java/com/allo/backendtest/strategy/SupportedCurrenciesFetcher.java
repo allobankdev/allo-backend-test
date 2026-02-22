008cdbcdc1380b14d4295dcbb02853d821b25ca5
fiqriturhamz <smandakotser@gmail.com>
2026-02-23 06:14:27 +0700

    feat: add historicallIdrUsdFetcher and SupportedCurrenciesFetcher

---
+++ src/main/java/com/allo/backendtest/strategy/SupportedCurrenciesFetcher.java
@@ -0,0 +1,49 @@
+package com.allo.backendtest.strategy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component("supported_currencies")
+public class SupportedCurrenciesFetcher implements IDRDataFetcher {
+
+    private static final Logger log = LoggerFactory.getLogger(SupportedCurrenciesFetcher.class);
+
+    private final WebClient webClient;
+
+    public SupportedCurrenciesFetcher(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @Override
+    public String getResourceType() {
+        return "supported_currencies";
+    }
+
+    @Override
+    public List<Object> fetchAndTransform() {
+
+        log.info("Fetching supported currencies...");
+
+        Map<String, String> response =
+                webClient.get()
+                        .uri("/currencies")
+                        .retrieve()
+                        .bodyToMono(Map.class)
+                        .block(Duration.ofSeconds(5));
+
+        return response.entrySet()
+                .stream()
+                .map(entry -> Map.of(
+                        "currency", entry.getKey(),
+                        "description", entry.getValue()
+                ))
+                .collect(Collectors.toList());
+    }
+}
