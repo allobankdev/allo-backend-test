d1919bb179aebfeb10d186d0340fa5f1f335b362
aditya-sukoco-aid <aditya.sukoco@akarintidata.ai>
2026-02-22 13:49:58 +0700

    feat: add loggings to spread factor repo

--- src/main/java/io/aditsukoco/allobank_test/repositories/spreadFactor/SpreadFactorDataRepositoryImpl.java
+++ src/main/java/io/aditsukoco/allobank_test/repositories/spreadFactor/SpreadFactorDataRepositoryImpl.java
@@ -1,7 +1,9 @@
 package io.aditsukoco.allobank_test.repositories.spreadFactor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 
+@Slf4j
 public class SpreadFactorDataRepositoryImpl implements SpreadFactorDataRepositoryInterface {
 
     private final double spreadFactor;
@@ -11,9 +13,11 @@ public class SpreadFactorDataRepositoryImpl implements SpreadFactorDataRepositor
         for (char a : githubUsername.toCharArray()) {
             unicodeSum += a;
         }
+        log.info("Unicode sum is " + unicodeSum);
 
         // Spread Factor = (Sum of Unicode Values % 1000) / 100000.0 (This will yield a unique factor between 0.00000 and 0.00999, ensuring a personalized result.)
         this.spreadFactor = (unicodeSum % 1000) / 100000.0;
+        log.info("Spread factor is " + this.spreadFactor);
     }
 
     @Override
