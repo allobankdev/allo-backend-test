9bf37d162df19cb3b698eb52f44dc6be46686c6f
Elviana Victoria <victoriaelviana@gmail.com>
2026-03-11 20:09:13 +0700

    Add finance controller

---
+++ src/main/java/com/allobank/financeapi/controller/FinanceController.java
@@ -0,0 +1,20 @@
+package com.allobank.financeapi.controller;
+
+import com.allobank.financeapi.model.FinanceData;
+import com.allobank.financeapi.service.FinanceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/finance/data")
+@RequiredArgsConstructor
+public class FinanceController {
+
+    private final FinanceService financeService;
+
+    @GetMapping("/{resourceType}")
+    public Mono<FinanceData> getData(@PathVariable String resourceType) {
+        return financeService.getDataFromStore(resourceType);
+    }
+}
