a78d2c5534424e2a093bd4d7fb3a6fe60e13f0a2
Hendra.Irawan <900571@hq.bni.co.id>
2026-03-14 13:24:14 +0700

    feat: implement historical_idr_usd strategy

---
+++ src/main/java/com/allobank/allobackendtest/strategy/impl/HistoricalRatesStrategy.java
@@ -0,0 +1,47 @@
+package com.allobank.allobackendtest.strategy.impl;
+
+import com.allobank.allobackendtest.strategy.IDRDataFetcher;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Component("historical_idr_usd")
+@RequiredArgsConstructor
+@Slf4j
+public class HistoricalRatesStrategy implements IDRDataFetcher {
+    private final WebClient webClient;
+    @Override
+    public Mono<Object> fetchData() {
+        return webClient.get()
+                .uri("/2024-01-01..2024-01-05?from=IDR&to=USD")
+                .retrieve()
+                .bodyToMono(Map.class)
+                .map(response -> {
+                    Map<String, Map<String, Double>> rates= (Map<String, Map<String, Double>>) response.get("rates");
+                    List<Map<String, Object>> resultList = new ArrayList<>();
+
+                    if (rates != null){
+                        rates.forEach((date, currencyMap) -> {
+                            Map<String, Object> entry = Map.of(
+                                    "date", date,
+                                    "rate_USD", currencyMap.get("USD")
+                            );
+                            resultList.add(entry);
+                        });
+                    }
+                    return resultList;
+                });
+
+    }
+
+    @Override
+    public boolean supports(String type) {
+        return "historical_idr_usd".equals(type);
+    }
+}
