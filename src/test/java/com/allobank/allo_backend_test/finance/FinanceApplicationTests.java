5209c3854919a9adc2b22bb16c2d4e61710d076e
Zidan Kharisma Adidarma <zidankharismaadidarma@gmail.com>
2026-03-26 14:19:13 +0700

    chore: init project with spring initializr

---
+++ src/test/java/com/allobank/allo_backend_test/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allobank.allo_backend_test.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
