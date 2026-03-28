6f797eda487aa2dbe9f3f790be179b8ed3dff8f5
Zidan Kharisma Adidarma <zidankharismaadidarma@gmail.com>
2026-03-28 07:16:05 +0700

    refactor[ConstraintA]: preload data to use registry and service

--- src/main/java/com/allobank/allo_backend_test/finance/runner/PreloadData.java
+++ src/main/java/com/allobank/allo_backend_test/finance/runner/PreloadData.java
@@ -1,11 +1,7 @@
 package com.allobank.allo_backend_test.finance.runner;
 
-import com.allobank.allo_backend_test.finance.client.DataSourceClient;
-import com.allobank.allo_backend_test.finance.model.CurrenciesModel;
-import com.allobank.allo_backend_test.finance.model.HistoricalRatesModel;
-import com.allobank.allo_backend_test.finance.model.LatestRatesModel;
-import com.allobank.allo_backend_test.finance.repository.FinanceRepository;
-import com.allobank.allo_backend_test.finance.service.SpreadService;
+import com.allobank.allo_backend_test.finance.service.FinanceService;
+import com.allobank.allo_backend_test.finance.service.strategy.FinanceResourceRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
@@ -17,39 +13,16 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PreloadData implements ApplicationRunner {
 
-    private final DataSourceClient client;
-    private final FinanceRepository repository;
-    private final SpreadService spreadService;
+    private final FinanceService financeService;
+    private final FinanceResourceRegistry registry;
 
     @Override
     public void run(ApplicationArguments args) {
-        fetchLatestRates();
-        fetchHistoricalRates();
-        fetchCurrencies();
-        log.info("Data Repository: '{}'", repository.getData());
-    }
-
-    private void fetchLatestRates() {
-        var dto = client.getLatestRates("IDR");
-        Double spread = spreadService.calculateSpread(dto.rates().get("USD"));
-        LatestRatesModel model = new LatestRatesModel(
-                dto.amount(), dto.base(), dto.date(), dto.rates(), spread);
-        repository.put(model.resourceType(), model);
-        log.info("preload {}", model.resourceType());
-    }
-
-    private void fetchHistoricalRates() {
-        var dto = client.getHistoricalRates("2024-01-01", "2024-01-05", "IDR", "USD");
-        HistoricalRatesModel model = new HistoricalRatesModel(
-                dto.amount(), dto.base(), dto.startDate(), dto.endDate(), dto.rates());
-        repository.put(model.resourceType(), model);
-        log.info("preload {}", model.resourceType());
-    }
-
-    private void fetchCurrencies() {
-        var dto = client.getCurrencies();
-        CurrenciesModel model = new CurrenciesModel(dto);
-        repository.put(model.resourceType(), model);
-        log.info("preload {}", model.resourceType());
+        log.info("finance data preload");
+        for (String resourceType : registry.getHandlerMap().keySet()) {
+            financeService.fetchByResourceType(resourceType);
+            log.info("Preloaded: {}", resourceType);
+        }
+        log.info("Finance data preload complete.");
     }
 }
--- src/main/java/com/allobank/allo_backend_test/finance/service/strategy/FinanceResourceRegistry.java
+++ src/main/java/com/allobank/allo_backend_test/finance/service/strategy/FinanceResourceRegistry.java
@@ -1,13 +1,16 @@
 package com.allobank.allo_backend_test.finance.service.strategy;
 
 import com.allobank.allo_backend_test.finance.service.strategy.finance_resource.FinanceResourceHandler;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Component
+@Getter
 public class FinanceResourceRegistry {
     private final Map<String, FinanceResourceHandler> handlerMap;
 
@@ -16,10 +19,6 @@ public class FinanceResourceRegistry {
         for (FinanceResourceHandler handler : handlers) {
             handlerMap.put(handler.resourceType(), handler);
         }
-    }
-
-    public Map<String, FinanceResourceHandler> asMap() {
-        return handlerMap;
     }
 
     public FinanceResourceHandler get(String resourceType) {
