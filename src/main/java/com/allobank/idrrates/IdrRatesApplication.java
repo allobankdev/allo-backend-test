d5a83d2199b72796b652aa3b57c95f91bc6d9823
FarizMR <frzrzky@gmail.com>
2026-03-29 22:15:52 +0700

    chore: init Spring Boot project

---
+++ src/main/java/com/allobank/idrrates/IdrRatesApplication.java
@@ -0,0 +1,13 @@
+package com.allobank.idrrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdrRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdrRatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allobank/idrrates/IdrRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allobank.idrrates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdrRatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
