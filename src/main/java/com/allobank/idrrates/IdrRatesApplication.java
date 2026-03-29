d2757a5066b4a6f3116e942c8b30d6fdbf69cc04
FarizMR <frzrzky@gmail.com>
2026-03-29 22:49:05 +0700

    feat: implement WebClientFactoryBean configuration

--- src/main/java/com/allobank/idrrates/IdrRatesApplication.java
+++ src/main/java/com/allobank/idrrates/IdrRatesApplication.java
@@ -1,13 +1,19 @@
 package com.allobank.idrrates;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IdrRatesApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(IdrRatesApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(IdrRatesApplication.class, args);
+		log.info("Spring boot application started");
 	}
 
 }
