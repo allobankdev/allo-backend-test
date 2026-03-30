768689190cc08d966d2bea76be230c79ad8e357f
FarizMR <frzrzky@gmail.com>
2026-03-30 23:23:09 +0700

    feat: fix CurrenciesDTO serialization

--- src/main/java/com/allobank/idrrates/dto/CurrenciesDTO.java
+++ src/main/java/com/allobank/idrrates/dto/CurrenciesDTO.java
@@ -1,14 +1,27 @@
 package com.allobank.idrrates.dto;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CurrenciesDTO {
-    private Map<String, String> currencies;
+    private Map<String, String> currencies = new HashMap<>();
+
+    @JsonAnySetter
+    public void setCurrency(String key, String value) {
+        currencies.put(key, value);
+    }
+
+    @JsonAnyGetter
+    public Map<String, String> getCurrencies() {
+        return currencies;
+    }
 }
