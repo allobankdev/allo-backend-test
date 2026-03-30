0a9a20c4e91101ee6e143100d4cff72b8645f8cf
FarizMR <frzrzky@gmail.com>
2026-03-30 23:23:21 +0700

    feat: implement FinanceController

---
+++ src/main/java/com/allobank/idrrates/controller/FinanceController.java
@@ -0,0 +1,32 @@
+package com.allobank.idrrates.controller;
+
+import com.allobank.idrrates.store.DataStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/finance")
+public class FinanceController {
+
+    private static final Logger log = LoggerFactory.getLogger(FinanceController.class);
+
+    @Autowired
+    private DataStore dataStore;
+
+    @GetMapping("/data/{resourceType}")
+    public ResponseEntity<Object> getData(@PathVariable String resourceType) {
+        log.info("Received request for resource type: {}", resourceType);
+        Object data = dataStore.get(resourceType);
+        if (data == null) {
+            log.warn("Resource type not found: {}", resourceType);
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(data);
+    }
+}
