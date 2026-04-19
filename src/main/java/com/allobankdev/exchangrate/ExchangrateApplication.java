bbf4e297efece78cab8fd7cccaf4901cf8fe3776
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2026-04-19 12:43:39 +0700

    chore: initial spring boot project setup

---
+++ src/main/java/com/allobankdev/exchangrate/ExchangrateApplication.java
@@ -0,0 +1,13 @@
+package com.allobankdev.exchangrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangrateApplication.class, args);
+	}
+
+}
