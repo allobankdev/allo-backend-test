25a269ae9a9dcc24d7d0d477dacc6542ad7dfc8a
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2026-04-19 14:22:05 +0700

    fix(dto): add annotation setter getter

--- src/main/java/com/allobankdev/exchangrate/dto/CurrencyResponse.java
+++ src/main/java/com/allobankdev/exchangrate/dto/CurrencyResponse.java
@@ -1,4 +1,9 @@
 package com.allobankdev.exchangrate.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class CurrencyResponse extends java.util.HashMap<String, String> {
 }
--- src/main/java/com/allobankdev/exchangrate/dto/HistoricalResponse.java
+++ src/main/java/com/allobankdev/exchangrate/dto/HistoricalResponse.java
@@ -3,9 +3,13 @@ package com.allobankdev.exchangrate.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Setter
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class HistoricalResponse {
     private int amount;
