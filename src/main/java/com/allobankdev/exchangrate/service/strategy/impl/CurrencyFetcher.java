c25b679928f6de04ed626696451cb0dc6990661c
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2026-04-19 13:28:05 +0700

    feat(strategy): implement supported currencies fetcher

---
+++ src/main/java/com/allobankdev/exchangrate/service/strategy/impl/CurrencyFetcher.java
@@ -0,0 +1,23 @@
+package com.allobankdev.exchangrate.service.strategy.impl;
+
+import com.allobankdev.exchangrate.client.ApiClient;
+import com.allobankdev.exchangrate.service.strategy.IdrDataFetcher;
+
+public class CurrencyFetcher implements IdrDataFetcher {
+    private final ApiClient client;
+    private final static String TYPE = "supported_currencies";
+
+    public CurrencyFetcher(ApiClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+
+    @Override
+    public Object fetch() {
+        return client.getCurrencies();
+    }
+}
