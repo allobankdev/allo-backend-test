b417a7403ed045b2e3f6918492834bef4162ae8e
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2026-04-19 13:13:09 +0700

    feat(client): add Frankfurter Api client integration

---
+++ src/main/java/com/allobankdev/exchangrate/client/ApiClient.java
@@ -0,0 +1,46 @@
+package com.allobankdev.exchangrate.client;
+
+import com.allobankdev.exchangrate.dto.CurrencyResponse;
+import com.allobankdev.exchangrate.dto.HistoricalResponse;
+import com.allobankdev.exchangrate.dto.LatestRateResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ApiClient {
+    private final RestTemplate restTemplate;
+
+    private final static String LATEST_RATES_ENDPOINT = "/latest?base-IDR";
+    private final static String HISTORICAL_RATES_ENDPOINT = "/2024-01-01..2024-01-05?from=IDR&to=USD";
+    private final static String CURRENCIES_ENDPOINT = "/currencies";
+
+    @Value("${external.api.base-url}")
+    private String baseUrl;
+
+    public ApiClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public LatestRateResponse getLatestRates() {
+        return restTemplate.getForObject(
+                baseUrl + LATEST_RATES_ENDPOINT,
+                LatestRateResponse.class
+        );
+    }
+
+    public HistoricalResponse getHistoricalRates() {
+        return restTemplate.getForObject(
+                baseUrl + HISTORICAL_RATES_ENDPOINT,
+                HistoricalResponse.class
+        );
+    }
+
+    public CurrencyResponse getCurrencies() {
+        return restTemplate.getForObject(
+                baseUrl + CURRENCIES_ENDPOINT,
+                CurrencyResponse.class
+        );
+    }
+
+}
