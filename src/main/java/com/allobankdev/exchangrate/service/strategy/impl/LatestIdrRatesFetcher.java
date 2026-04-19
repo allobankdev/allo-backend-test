6ef1ce394e0cd871e85b4114527472ae57623e00
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2026-04-19 13:42:57 +0700

    feat(strategy): implement latest IDR rates fetcher

---
+++ src/main/java/com/allobankdev/exchangrate/service/strategy/impl/LatestIdrRatesFetcher.java
@@ -0,0 +1,46 @@
+package com.allobankdev.exchangrate.service.strategy.impl;
+
+import com.allobankdev.exchangrate.client.ApiClient;
+import com.allobankdev.exchangrate.service.strategy.IdrDataFetcher;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class LatestIdrRatesFetcher implements IdrDataFetcher {
+    private final ApiClient client;
+    private final static String TYPE = "latest_idr_rates";
+    private final static BigDecimal DIVISOR = BigDecimal.valueOf(100000);
+
+    @Value("${app.github.username}")
+    private String githubUsername;
+
+    public LatestIdrRatesFetcher(ApiClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+
+    @Override
+    public Object fetch() {
+        var data =  client.getLatestRates();
+        BigDecimal usdRate = data.getRates().get("USD");
+
+        BigDecimal spread = calculateSpread(githubUsername);
+        BigDecimal result = BigDecimal.ONE.divide(usdRate, RoundingMode.HALF_UP).multiply(BigDecimal.ONE.add(spread));
+        data.setUsdBuySpreadIdr(result);
+
+        return data;
+    }
+
+    private BigDecimal calculateSpread(String username) {
+        int sum = username.chars().sum();
+        int mod = sum % 1000;
+
+        return BigDecimal.valueOf(mod)
+                .divide(DIVISOR, 6, RoundingMode.HALF_UP);
+    }
+}
