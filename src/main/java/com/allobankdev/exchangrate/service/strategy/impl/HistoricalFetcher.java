24ecde629e5094d0fba7b14d3d85680f97eb286d
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2026-04-19 13:31:49 +0700

    feat(strategy): implement historical rates IDR USD fetcher

---
+++ src/main/java/com/allobankdev/exchangrate/service/strategy/impl/HistoricalFetcher.java
@@ -0,0 +1,23 @@
+package com.allobankdev.exchangrate.service.strategy.impl;
+
+import com.allobankdev.exchangrate.client.ApiClient;
+import com.allobankdev.exchangrate.service.strategy.IdrDataFetcher;
+
+public class HistoricalFetcher implements IdrDataFetcher {
+    private final ApiClient client;
+    private final static String TYPE = "historical_idr_usd";
+
+    public HistoricalFetcher (ApiClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+
+    @Override
+    public Object fetch() {
+        return client.getHistoricalRates();
+    }
+}
