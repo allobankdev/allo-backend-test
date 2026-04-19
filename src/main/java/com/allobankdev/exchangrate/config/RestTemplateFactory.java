fb62cd1bb0417dfcb892f30fda62cb85791ad2d3
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2026-04-19 16:45:00 +0700

    refactor(config): add timeout and header configuration

--- src/main/java/com/allobankdev/exchangrate/config/RestTemplateFactory.java
+++ src/main/java/com/allobankdev/exchangrate/config/RestTemplateFactory.java
@@ -1,16 +1,29 @@
 package com.allobankdev.exchangrate.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@Slf4j
 public class RestTemplateFactory implements FactoryBean<RestTemplate> {
 
     @Override
     public @Nullable RestTemplate getObject() throws Exception {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+
+        RestTemplate restTemplate = new RestTemplate(factory);
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            request.getHeaders().add("Accept", "application/json");
+            log.info("Outgoing request: {} {}", request.getMethod(), request.getURI());
+            return execution.execute(request, body);
+        });
+        return restTemplate;
     }
 
     @Override
